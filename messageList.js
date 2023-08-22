b38f9deff57200351ebfad692bd9962c392b6c2d
StevieG46 <126635670+StevieG46@users.noreply.github.com>
2023-08-22 20:40:08 +1000

    feature add 42 guesser app

---
+++ messageList.js
@@ -0,0 +1,15 @@
+let messages = [
+    "hello Callum",
+    "hello naomi",
+    "hello nick",
+    "hellow luke!"
+]
+
+function sayMessages(){
+    console.log(messages);
+}
+sayMessages();
+
+module.exports = {
+    messagesToShare: messages
+}
